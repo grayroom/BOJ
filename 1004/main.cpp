8c40a72405d4bda652274274e668530c661a9072
JeongHoon Lee <leejh9226@gmail.com>
2020-01-30 12:58:45 +0900

    1004::어린왕자::failed

---
+++ 1004/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int testCase{};
+    cin >> testCase;
+
+    int 
+        path[4]{},
+        numPlanet{},
+        **planetInfo{},
+        startDistSqr{},
+        finishDistSqr{},
+        rangeSqr{};
+
+    vector<int> res{};
+
+    for(int i = 0; i < testCase; i++) {
+        for(int j = 0; j < 4; j++) {
+            cin >> path[j];
+        }
+
+        cin >> numPlanet;
+        planetInfo = new int*[numPlanet];
+        for(int j = 0; j < numPlanet; j++) {
+            planetInfo[j] = new int[3];
+        }
+
+        for(int j = 0; j < numPlanet; j++) {
+            for(int k = 0; k < 3; k++) {
+                cin >> planetInfo[j][k];
+            }
+        }
+
+        int passCount{};
+
+        for(int j = 0; j < numPlanet; j++) {
+            startDistSqr = (path[0] - planetInfo[j][0]) * (path[0] - planetInfo[j][0]) +
+                           (path[1] - planetInfo[j][1]) * (path[1] - planetInfo[j][1]);
+            finishDistSqr = (path[2] - planetInfo[j][0]) * (path[2] - planetInfo[j][0]) +
+                            (path[3] - planetInfo[j][1]) * (path[3] - planetInfo[j][1]);
+            rangeSqr = planetInfo[j][2] * planetInfo[j][2];
+            
+            if((startDistSqr - rangeSqr) * (finishDistSqr - rangeSqr) < 0) {
+                passCount++;
+            }
+        }
+
+        res.push_back(passCount);
+    }
+
+    for(int i = 0; i < res.size(); i++) {
+        if(i != res.size() - 1) {
+            cout << res[i] << endl;
+        } else {
+            cout << res[i];
+        }
+    }
+
+    return 0;
+}
