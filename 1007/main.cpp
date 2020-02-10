8bc4219dd8eb31410cb8d7fc5c65e042deaabd6a
JeongHoon Lee <leejh9226@gmail.com>
2020-02-10 14:53:30 +0900

    1007::벡터매칭::progress

--- 1007/main.cpp
+++ 1007/main.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <set>
-#include <utility>
-#include <cmath>
+#include <random>
 #include <iomanip>
 
+
 using namespace std;
-
-pair<int, int>& findClosest(vector<pair<int, int>>& P, set<pair<int, int>>& V, const pair<int, int> src);
-double getDist(pair<int, int> from, pair<int, int> to);
-
 
 int main() {
     int T{},
@@ -22,66 +18,79 @@ int main() {
     cin >> T;
     for(int i = 0; i < T; ++i) {
         vector<pair<int, int>> P{};
-        set<pair<int, int>> V{};
-        pair<int, int> fromNode{},
-                       toNode{};
 
-        double resTemp{};
-
+        // N개의 정점 P를 입력합니다.
         cin >> N;
         for(int j = 0; j < N; ++j) {
             cin >> X >> Y;
             P.emplace_back(X, Y);
         }
 
-        for(auto iter = P.begin(); iter != P.end() || P.size() != V.size(); iter++) {
-            if(V.find(*iter) == V.end()) {
-                fromNode = *iter;
-                V.emplace(fromNode);
-                toNode = findClosest(P, V, *iter);
-                V.emplace(toNode);
 
-                resTemp += getDist(fromNode, toNode);
-            } else {
-                continue;
-            }
-        }
-
-        res.push_back(resTemp);
-    }
-
-    cout << fixed << setprecision(12);
-    for(double x : res) {
-        cout << x << endl;
+        // 1.   set P에서 2개의 정점을 선택하여 하나의 벡터를 만든다
+        // 1-1. 해당 벡터를 "사용한 벡터 set"에 포함시킨다.
     }
 
     return 0;
 }
 
-pair<int, int>& findClosest(vector<pair<int, int>>& P, set<pair<int, int>>& V, const pair<int, int> src) {
-    auto iter = P.begin();
+double findMinVectorSet(vector<pair<int, int>>& P) {
+    vector<pair<int, int>> V{};
 
-    double distMin{},
-           distTemp{};
 
-    pair<int, int>& closest {*iter};
-
-    for(pair<int ,int> x : P) {
-        if(V.find(x) == V.end()) {
-            distTemp = pow((x.first - src.first), 2) + pow((x.second - src.second), 2);
-
-            if (distTemp < distMin || (distMin == 0 && distTemp != 0)) {
-                distMin = distTemp;
-                closest = x;
-            }
-        } else {
-            continue;
-        }
-    }
-
-    return closest;
 }
 
-double getDist(pair<int, int> from, pair<int, int> to) {
-    return sqrt(pow((from.first - to.first), 2) + pow((from.second - to.second), 2));
+void selectLoop(vector<pair<int, int>>& P, set<int>& usedP, vector<pair<int, int>>& V, set<pair<int, int>>& usedV, double& minDist, const int level = 0) {
+    int posX{},
+        posY{};
+
+    minDist = 400000;
+
+    if(level != P.size() / 2) {
+        random_device rd;
+        mt19937 mersenne(rd());
+        uniform_int_distribution<> rg(0, 19);
+
+        for (int i = 0; i < (P.size() - level) * (P.size() - level - 1) / 2;) {
+            if(level == 0) {
+                usedP.clear();
+            }
+
+            do {
+                posX = rg(mersenne);
+                posY = rg(mersenne);
+            } while (posX == posY &&
+                     usedP.find(posX) != usedP.end() &&
+                     usedP.find(posY) != usedP.end() &&
+                     usedV.find(make_pair(posX, posY)) != usedV.end());
+
+            usedP.emplace(posX);
+            usedP.emplace(posY);
+            usedV.emplace(make_pair(posX, posY));
+
+            V.emplace_back(make_pair(posX, posY));
+
+
+            selectLoop(P, usedP, V, usedV, minDist, ++i);
+        }
+    } else {
+        pair<int, int> firstP{},
+                       secondP{};
+
+        for(pair<int, int> currV : V) {
+            firstP = P[currV.first];
+            secondP = P[currV.second];
+
+            posX -= firstP.first;
+            posX += secondP.first;
+            posY -= firstP.second;
+            posY += secondP.second;
+        }
+
+        if (sqrt(pow(posX, 2) + pow(posY, 2)) < minDist) {
+            minDist = sqrt(pow(posX, 2) + pow(posY, 2));
+        }
+
+        V.clear();
+    }
 }
