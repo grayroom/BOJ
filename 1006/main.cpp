6aabeb60ce284e8953a30362f035fcbbc1e36dce
JeongHoon Lee <leejh9226@gmail.com>
2020-02-11 15:30:59 +0900

    1006::습격자초라기::progress

--- 1006/main.cpp
+++ 1006/main.cpp
@@ -63,60 +63,8 @@ int main() {
         cout << endl << "----------------------" << endl;
         */
 
-        int left{}, right{}, up{}, min{};
-        // 카운트 결과를 보고, 인접한 두 구역의 합이 작은 순서대로 두 구역을 커버하고, 해당 구역의 카운트를 모두 제거합니다.
-        for(int j = 1; j <= 3; ++j) { // 카운트 횟수가 작은 구역부터 탐색
-            for(int k = 0; k < N * 2; ++k) { // 모든 구역을 탐색
-                if(roomCnt[k / N][k % N] == j) { // 현재 탐색중인 카운트 횟수와 일치하는 구역을 찾은 경우
-                    // 인접한 3개의 구역의 카운트를 확인(카운트가 0인 경우, 해당 구역은 카운트 4를 가졌던것으로 취급)
-                    up = roomCnt[(k / N + 1) % 2][k % N];
-                    if(up == 0) {
-                        up = 4;
-                    }
-                    right = roomCnt[k / N][(k + 1) % N];
-                    if(right == 0) {
-                        right = 4;
-                    }
-                    left = roomCnt[k / N][(k + N - 1) % N];
-                    if(left == 0) {
-                        left = 4;
-                    }
-
-                    if(up == right && up == left && up == 4) { // 인접한 모든 구역의 카운트가 0인 경우, 해당 구역은 하나의 소대로 커버해야함 따라서 카운트를 삭제하지 않음
-                        continue;
-                    } else { // 인접한 구역중 적어도 하나의 구역의 카운트가 0이 아닌경우, 0이 아닌 가장작은 카운트를 갖는 구역과 현재 탐색중인 구역을 하나의 소대로 커버한다.
-                        min = std::min(left, right);
-                        min = std::min(min, up);
-
-                        // 카운트를 제거
-                        if(min == up && W >= room[k / N][k % N] + room[((k + N) / N) % 2][k % N]) {
-                            roomCnt[k / N][k % N] = 0;
-                            roomCnt[(k / N + 1) % 2][k % N] = 0;
-                            res++;
-                            duoCount++;
-
-                            continue;
-                        } else if (std::min(left, right) == right && W >= room[k / N][k % N] + room[k / N][(k + 1) % N]) {
-                            roomCnt[k / N][k % N] = 0;
-                            roomCnt[k / N][(k + 1) % N] = 0;
-                            res++;
-                            duoCount++;
-
-                            continue;
-                        } else if (W >= room[k / N][k % N] + room[k / N][(k + N - 1) % N]) {
-                            roomCnt[k / N][k % N] = 0;
-                            roomCnt[k / N][(k + N - 1) % N] = 0;
-                            res++;
-                            duoCount++;
-
-                            continue;
-                        } else {
-                            continue;
-                        }
-                    }
-                }
-            }
-        }
+        // 1. 두 구역을 커버할 수 있다면, 그 조합을 저장한다.
+        // 2. 저장한 조합 중에서 최대한 많이 사용할 수 있는 새로운 조합을 찾는다. (전수조사, 새로운방법)
 
         res += N * 2 - duoCount * 2;
         result.emplace_back(res);
@@ -131,7 +79,6 @@ int main() {
         delete[] room;
     }
 
-    cout << endl << "----------------------" << endl;
     for(int x : result) {
         cout << x << endl;
     }
