606005746f8b3ca09fbc2a4ab9896f1c22594ec2
JeongHoon Lee <leejh9226@gmail.com>
2020-02-21 18:25:52 +0900

    11003::최솟값찾기::progress

--- 11003/main.cpp
+++ 11003/main.cpp
@@ -1,57 +1,53 @@
 #include <iostream>
-#include <cmath>
 #include <vector>
 #include <algorithm>
 
+inline void build(vector<int>& tree, int N);
+inline int getMin(int left, int right);
+
 using namespace std;
 
-inline int init(vector<int>& a, vector<int>& tree, int node, int start, int end);
-inline int find(vector<int>& tree, int node, int start, int end, int left, int right);
-
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL);    
 
     int N{}, L{};
     cin >> N >> L;
 
-    vector<int> a(N + 1),
-            tree((1 << ((int)ceil(log2(N)) + 1)));
+    vector<int> tree(2 * N);
     for(int i = 0; i < N; ++i) {
-        cin >> a[i + 1];
+        cin >> tree[N + i];
     }
-    init(a, tree, 1, 1, N);
+    build(tree, N);
 
-
-    for(int i = 1; i <= N; ++i) {
-        if(i-L+1 >= 1) {
-            cout << find(tree, 1, 1, N, i-L+1, i) << ' ';
+    for(int i = 0; i < N; ++i) {
+        if(i - L + 1 >= 0) {
+            cout << getMin(tree, N, i - L + 1, i) << ' ';
         } else {
-            cout << find(tree, 1, 1, N, 1, i) << ' ';
+            cout << getMin(tree, N, 0, i) << ' ';
+        }
+    }
+}
+
+inline void build(vector<int>& tree, int N) {
+    for(int i = N - 1; i > 0; --i) {
+        tree[i] = tree[i << 1] + tree[i << 1 | 1];
+    }
+}
+
+inline int getMin(vector<int>& tree, int N, int left, int right) {
+    int res = numeric_limits<int>::max();
+
+    for(left += N, right += N; left <= right; left >>= 1, right >>= 1) {
+        if(left == right) {
+            return res = min(res, tree[left]);
+        }
+        if(left & 1) {
+            res = min(res, tree[left]), ++left;
+        }
+        if(right & 1) {
+            res = min(res, tree[right]), ++right;
         }
     }
 
-    return 0;
-}
-
-// FIXME: 재귀함수 반복문으로 바꾸기
-inline int init(vector<int>& a, vector<int>& tree, int node, int start, int end) {
-    if(start == end) {
-        return tree[node] = a[start];
-    } else {
-        int left{init(a, tree, node * 2, start, (start + end) / 2)},
-            right{init(a, tree, node * 2 + 1, (start + end) / 2 + 1 , end)};
-
-        return tree[node] = min(left, right);
-    }
-}
-
-inline int find(vector<int>& tree, int node, int start, int end, int left, int right) {
-    if(left > end || right < start) {
-        return numeric_limits<int>::max();
-    }
-    if(left <= start && end <= right) {
-        return tree[node];
-    }
-    return min(find(tree, node * 2, start, (start + end) / 2, left, right),
-           find(tree, node * 2 + 1, (start + end) / 2 + 1, end, left, right));
+    return res;
 }
